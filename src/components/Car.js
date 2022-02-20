21ec2b69237ff7fac6c854e04b4396e10a46e0ef
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2022-02-20 19:38:28 +0300

    Avoid this mistake!!

--- src/components/Car.js
+++ src/components/Car.js
@@ -2,10 +2,15 @@ function Car(props){
     const startCar=(displayingMessage, event)=>{
         alert(event.type);
     }
+    const stopCar=()=>{
+        alert("Stopped!!");
+    }
     return(
         <>
          <h1>Hi, This is a component for a {props.brand.carName} - {props.brand.carModel}</h1>
          <button onClick={(event)=>startCar("Car is going slowly!!", event)}> Start Car </button>
+         <button onClick={stopCar}>To Stop the Car</button>
+        {/* there is a difference between using stopCar and stopCar().  */}
         </>
     )
 }
