e3f3a15823e9bb2f9919cc307030367086622ee2
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2022-02-20 19:30:45 +0300

    add event handler to have access to the React event that triggered the function
    it will going to be displayed "click"

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,11 +1,11 @@
 function Car(props){
-    const startCar=(displayingMessage)=>{
-        alert(displayingMessage);
+    const startCar=(displayingMessage, event)=>{
+        alert(event.type);
     }
     return(
         <>
          <h1>Hi, This is a component for a {props.brand.carName} - {props.brand.carModel}</h1>
-         <button onClick={()=>startCar("Car is going fast!!")}> Start Car </button>
+         <button onClick={(event)=>startCar("Car is going slowly!!", event)}> Start Car </button>
         </>
     )
 }
