1b7ddff7f8719acb68693a703c714da8e6df975f
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2022-02-20 19:21:46 +0300

    add onClick Event

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,4 +1,12 @@
 function Car(props){
-    return <h1>Hi, This is a component for a {props.brand.carName} - {props.brand.carModel}</h1>
+    const startCar=()=>{
+        alert("Car is going!!");
+    }
+    return(
+        <>
+         <h1>Hi, This is a component for a {props.brand.carName} - {props.brand.carModel}</h1>
+         <button onClick={startCar}> Start Car </button>
+        </>
+    )
 }
 export default Car;
