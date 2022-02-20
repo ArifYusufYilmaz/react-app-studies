16b9b8c287f0a2881115e73ec4ba26d2d5e82294
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2022-02-20 18:31:33 +0300

    add components Car.js, Garage.js and import it to App.js

---
+++ src/components/Car.js
@@ -0,0 +1,4 @@
+function Car(){
+    return <h1>Hi, This is a component for a TOGG CAR</h1>
+}
+export default Car;
---
+++ src/components/Garage.js
@@ -0,0 +1,13 @@
+import Car from './Car.js';
+
+function Garage(){
+    return (
+        <>
+        <p>Whic car is in the garage?</p>
+        <Car></Car>
+        </>
+    );
+
+    
+}
+export default Garage;
