eabb6a4b84e8c1333ec3f99f9b1b410e9f52fe07
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2022-02-21 01:26:25 +0300

    update the List version

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,42 +1,11 @@
 function Car(props){
-    const isStarted = props.brand.carSituation;
-    const carMaintenance = props.brand.carMaintenance;
-    const startCar=(displayingMessage, event)=>{
-        alert(event.type);
-    }
-    const stopCar=()=>{
-        alert("The Car is stopping!!");
-    }
-    if(isStarted){
         return(
             <>
-             <h1>Hi, This is a component for a {props.brand.carName} - {props.brand.carModel}</h1>
-             <button onClick={stopCar}>To Stop the Car</button>
-             {carMaintenance.length > 2 &&
-                <h2>
-                    The count of maintenance is {carMaintenance.length}
-                </h2>
-             }
-             <b>Cars are : </b> <br/>
-            <ul>
-                {props.brand.cars.map((car)=> <li>{car}</li>)}
+             <ul>
+             {props.carInfo.map((car)=> <li>{car.brand} - {car.carModel}</li>)}
+            
             </ul>
             </>
         )
-    }
-    else{
-            return(
-            <>
-             <h1>Hi, This is a component for a {props.brand.carName} - {props.brand.carModel}</h1>
-             <button onClick={(event)=>startCar("Car is starting!!", event)}> Start Car </button>
-             {carMaintenance.length > 4 &&
-                <h2>
-                    The count of maintenance is {carMaintenance.length}
-                </h2>
-             }
-              </>
-            )
-    }
-    
 }
 export default Car;
--- src/components/Garage.js
+++ src/components/Garage.js
@@ -1,13 +1,18 @@
 import Car from './Car.js';
 
 function Garage(){
-    
-    const carInfo ={carName: "Togg Car", carModel:"2022", carSituation:true, carMaintenance:[2015,2016,2017],cars : ["Togg", "Ford", "Toyota", "Karsan","Tofaş","Tesla"]};
+    const cars = [
+        {id:1, brand:"Togg"  ,carModel:"2023",carSituation:true,carMaintenance:[]},
+        {id:2, brand:"Ford"  ,carModel:"2018",carSituation:true,carMaintenance:[2019]},
+        {id:3, brand:"Toyota",carModel:"2013",carSituation:true,carMaintenance:[2015,2017,2018,2019]},
+        {id:4, brand:"Karsan",carModel:"2011",carSituation:true,carMaintenance:[2013,2014,2018]},
+        {id:5, brand:"Tofaş" ,carModel:"1999",carSituation:true,carMaintenance:[2002,2005,2007,2015,2016,2018]},
+        {id:6, brand:"Tesla" ,carModel:"2022",carSituation:true,carMaintenance:[2023]}
+    ];
     return (
         <>
         <p>Whic car is in the garage?</p>
-        
-            <Car brand={carInfo}/>
+            <Car carInfo={cars}/>
         </>
     );
 
